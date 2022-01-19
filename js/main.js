2a12062c27c7cceae74e7fa472773c9392366bcf
Célio <celiofagundes30@gmail.com>
2022-01-19 16:54:13 -0300

    header finish

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$('.slider').slick({
+    dots:false,
+    infinite:false,
+    speed: 300,
+    slidesToShow: 1,
+    variableWidth: false,
+    centerMode: true,
+    arrows: false
+});
+function myFunction() {
+  let links = document.getElementById("myTopnav");
+  let btn = document.getElementById('menu-toggle')
+  body = document.querySelector("body");
+  if (links.className === "links") {
+    links.className += " responsive";
+    btn.className += " responsive";
+    body.style.backgroundImage = "linear-gradient(to bottom, "+ '#fff' +", "+ '#000' +")"
+  } else {
+    links.className = "links";
+    btn.className = "menu-icon";
+    body.style.backgroundImage = 'none';
+  }
+  }
+
--- js/slider.js
+++
@@ -1,9 +0,0 @@
-$('.slider').slick({
-    dots:false,
-    infinite:false,
-    speed: 300,
-    slidesToShow: 1,
-    variableWidth: false,
-    centerMode: true,
-    arrows: false
-});
